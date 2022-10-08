ef90f5e1f251f43f49e00cf6d5e46d8113aab407
Anselmo Parente Martins <anselmoparente@gmail.com>
2022-10-08 18:28:38 -0300

    Created laboratory model

--- lib/models/laboratory_model.dart
+++ lib/models/laboratory_model.dart
@@ -1,1 +1,33 @@
+import 'package:farmacoped/models/model.dart';
 
+class RoomModel implements Model {
+  final String amount;
+  final String composition;
+  final String warnings;
+
+  RoomModel({
+    required this.amount,
+    required this.composition,
+    required this.warnings,
+  });
+
+  RoomModel.fromMap(Map<String, dynamic> map)
+      : amount = map['amount'] is String ? map['amount'] as String : '',
+        composition =
+            map['composition'] is String ? map['composition'] as String : '',
+        warnings = map['warnings'] is String ? map['warnings'] as String : '';
+
+  @override
+  Map<String, dynamic> toMap() {
+    return {
+      'amount': amount,
+      'composition': composition,
+      'warnings': warnings,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'LaboratoryModel{quantidade: $amount, composição: $composition, avisos: $warnings}';
+  }
+}
---
+++ lib/models/model.dart
@@ -0,0 +1,6 @@
+abstract class Model {
+  Map<String, dynamic> toMap();
+
+  // ignore: avoid_unused_constructor_parameters
+  Model.fromMap(Map<String, dynamic> map);
+}
