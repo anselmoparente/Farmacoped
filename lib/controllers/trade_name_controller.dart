e93e3038030ce693b9a597f53059b531fd24c867
Anselmo Parente Martins <anselmoparente@gmail.com>
2022-10-21 01:30:27 -0300

    Remove unnecessary imports

--- lib/controllers/trade_name_controller.dart
+++ lib/controllers/trade_name_controller.dart
@@ -1,6 +1,4 @@
 import 'package:farmacoped/models/laboratory_model.dart';
-import 'package:farmacoped/models/medication_model.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class TradeNameController extends GetxController {
