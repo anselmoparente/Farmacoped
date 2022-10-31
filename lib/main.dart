6211dcdef9fef24317f55996a42e17227038c277
Anselmo Parente Martins <anselmoparente@gmail.com>
2022-10-31 16:54:29 -0300

    Add AdMob and set preferred orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:farmacoped/bindings/tutorial_binding.dart';
 import 'package:farmacoped/routes/app_pages.dart';
+import 'package:flutter/services.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -7,7 +9,11 @@ import 'package:get/get.dart';
 import 'package:farmacoped/translations/app_translations.dart';
 
 Future<void> main() async {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
