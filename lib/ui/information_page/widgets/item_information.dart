4624f1775d51d9506fbbe7acab4c4260a9682826
Anselmo Parente Martins <anselmoparente@gmail.com>
2023-03-23 08:15:03 -0300

    Update item information

--- lib/ui/information_page/widgets/item_information.dart
+++ lib/ui/information_page/widgets/item_information.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ItemInformation extends StatelessWidget {
   const ItemInformation({super.key});
@@ -6,11 +7,25 @@ class ItemInformation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Row(
+      child: Column(
         children: [
-          CircleAvatar(),
-          Column(
-            children: [],
+          Row(
+            children: [
+              CircleAvatar(),
+              Column(
+                children: [
+                  Text('Anselmo Parente Martins'),
+                  Container(
+                    height: 500,
+                    width: Get.width * 0.8,
+                    child: Text(
+                      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+                      maxLines: 500,
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ],
       ),
