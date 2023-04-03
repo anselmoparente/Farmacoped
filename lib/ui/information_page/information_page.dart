b35d66db618a1063922ac1aa9ed4c913e23d5e59
Anselmo Martins <anselmomartins@institutoiracema.org.br>
2023-04-03 16:53:48 -0300

    Update item information page

--- lib/ui/information_page/information_page.dart
+++ lib/ui/information_page/information_page.dart
@@ -7,6 +7,27 @@ class InformationPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Map<String, dynamic> info = {
+      'names': [
+        'Morgana Pontes Brasil Gradvohl',
+        'Francisco Xavier Paranhos Coêlho Simões',
+        'Letícia Brasil Gradvohl',
+        'Tássia Aguiar de Oliveira',
+      ],
+      'informations': [
+        'Graduada em Odontologia pela UFC\nEspecialista em Odontopediatria em Unicastelo\nEspecialista em Ortodontia pela Uningá\nEspecialista em hof pela Faipe\nMestre em Psicologia Unifor\nProfessora da área de clínica infantil da Unifor\nProfessora do projeto de pacientes especiais e mamãe bebê\nProfessora de residência em Hof',
+        'Graduado em odontologia pela Faculdade de Odontologia da Universidade Federal da Bahia\nEspecialista em Odontopediatria pela Associação Paulista de Cirurgiões-dentistas - Bauru\nMestre em Clínica Odontológica pela Faculdade de Odontologia da Universidade Federal da Bahia\nDoutor em Odontopediatria pela Faculdade de Odontologia da Universidade de São Paulo\nProfessor Titular em Odontopediatria do Curso de Odontologia da Universidade Estadual do Sudoeste da Bahia\nProfessor Adjunto do Curso de Odontologia da Escola Bahiana de Medicina e Saúde Pública',
+        'Acadêmica do 7º semestre de Medicina na Universidade de Fortaleza, atual vice presidente da Liga de Geriatria e Gerontologia da UNIFOR e atual bolsista FUNCAP de Iniciação Científica no Hospital Walter Cantídeo',
+        'Advogada e, atualmente, acadêmica do 10º semestre de Odontologia na Universidade de Fortaleza - Unifor, participo da Liga Nacional Acadêmica da ABOPED desde 2020. Fui monitora da disciplina de Clínica Infantil II durante os anos de 2020 a 2022.',
+      ],
+      'images': [
+        'scasc',
+        'dvkhadiova',
+        'avavava',
+        'evvdsd',
+      ],
+    };
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -19,8 +40,12 @@ class InformationPage extends StatelessWidget {
         ),
       ),
       body: ListView.builder(
-        itemCount: 5,
-        itemBuilder: (context, index) => ItemInformation(),
+        itemCount: info['names'].length,
+        itemBuilder: (context, index) => ItemInformation(
+          name: info['names'][index],
+          information: info['informations'][index],
+          image: info['images'][index],
+        ),
       ),
     );
   }
--- lib/ui/information_page/widgets/item_information.dart
+++ lib/ui/information_page/widgets/item_information.dart
@@ -1,33 +1,45 @@
+import 'dart:math';
+
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ItemInformation extends StatelessWidget {
-  const ItemInformation({super.key});
+  final String name;
+  final String information;
+  final String image;
+
+  const ItemInformation({
+    required this.name,
+    required this.information,
+    required this.image,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              CircleAvatar(),
-              Text(
-                'Anselmo Parente Martins',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-          Container(
-            height: 200,
-            width: Get.width * 0.8,
-            child: AutoSizeText(
-              'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+      child: Container(
+        width: Get.width * 0.8,
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CircleAvatar(
+                  child: Image.asset(
+                    'assets/images/splash_icon.png',
+                  ),
+                ),
+                Text(name, style: TextStyle(fontWeight: FontWeight.bold)),
+              ],
             ),
-          ),
-        ],
+            Container(
+              height: 200,
+              child: AutoSizeText(information),
+            ),
+          ],
+        ),
       ),
     );
   }
